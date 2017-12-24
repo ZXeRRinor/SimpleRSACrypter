63ae89eac28989cab8768e82395832804eb9f902
ZXeRRinor <antpotapov51@gmail.com>
2017-12-24 11:41:06 +0300

    Recreated to module. It can be loaded from other files.

--- prime_number_generator.rb
+++ prime_number_generator.rb
@@ -1,7 +1,9 @@
-def prime_numbers(lim)
-  nums = (2..lim).to_a #preparing
-  nums.each do |elem|
-    nums.delete_if {|i| (i > elem) && ((i % elem) == 0)}
+module PrimeNumberProcessor
+  def prime_numbers(lim)
+    nums = (2..lim).to_a #preparing
+    nums.each do |elem|
+      nums.delete_if {|i| (i > elem) && ((i % elem) == 0)}
+    end
+    nums
   end
-  nums
 end
